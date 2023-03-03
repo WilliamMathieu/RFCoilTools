220ee88b22b9f08d2e4d4d5e6dcc4f4599fc0464
WilliamMathieu <3903679+WilliamMathieu@users.noreply.github.com>
2023-03-02 23:35:48 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -12,17 +12,17 @@ setup(
     url='https://github.com/WilliamMathieu/RFCoilTools',
     py_modules=[],
     install_requires=[
-        'PyQt5==5.9.2',
         'numpy',
         'scipy',
-        'matplotlib',
         'ipython',
         'jupyter',
-        'pandas',
         'sympy',
         'nose',
         'dicom2nifti',
         'nibabel',
         'Pillow',
-        'scikit-rf']
+        'scikit-rf'
+        'matplotlib',
+        'pandas',
+        'PyQt5']
 )
